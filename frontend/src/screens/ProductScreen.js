6cb7bbab972e0a95c30d13228d8211ca357938be
BouabdellaouiRajae <rajae.bouabdellaoui@gmail.com>
2021-05-22 11:03:21 +0200

    second commit

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import { Link } from 'react-router-dom';
+import { detailsProduct } from '../actions/productActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import Rating from '../components/Rating';
-import data from '../data';
+
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
+  const dispatch = useDispatch();
+  const productId = props.match.params.id;
+  const [qty, setQty] = useState(1);
+  const productDetails = useSelector( state => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() =>{
+    dispatch(detailsProduct(productId));
+
+  }, [dispatch, productId ]);
+  const addToCartHandler = () =>{
+    props.history.push(`/cart/${productId}?qty=${qty}`);
   }
   return (
     <div>
+    {loading ? (
+      <LoadingBox></LoadingBox>
+    ) : error ? (
+      <MessageBox variant="danger">{error}</MessageBox>
+    ) : (
+      <div>
       <Link to="/">Back to result</Link>
       <div className="row top">
         <div className="col-2">
@@ -36,7 +55,7 @@ export default function ProductScreen(props) {
         <div className="col-1">
           <div className="card card-body">
             <ul>
-              <li>
+              <li> 
                 <div className="row">
                   <div>Price</div>
                   <div className="price">${product.price}</div>
@@ -54,13 +73,38 @@ export default function ProductScreen(props) {
                   </div>
                 </div>
               </li>
-              <li>
-                <button className="primary block">Add to Cart</button>
+              {
+                product.countInStock > 0 && (
+                  <>
+                  <li>
+                    <div className ="row">
+                      <div> Qty </div>
+                      <div>
+                        <select value= {qty} onChange= {e => setQty(e.target.value)}>
+                            {
+                              [...Array(product.countInStock).keys()].map((x) => (
+                                <option key={x + 1 }  value={ x+1 }>{x + 1}</option>
+                              ))
+                            }
+                        </select>
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                <button onClick={addToCartHandler}
+                 className="primary block">Add to Cart</button>
               </li>
+              </>
+                )
+              }
+              
             </ul>
           </div>
         </div>
       </div>
     </div>
+    )}
+  </div>
+    
   );
 }
