05497198fe2c5b8df71318ced73d77c881b8ac22
BouabdellaouiRajae <rajae.bouabdellaoui@gmail.com>
2021-05-24 14:05:19 +0200

    fourth commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,29 @@
 import { applyMiddleware, createStore, compose, combineReducers } from "redux";
 import thunk from 'redux-thunk';
 import { cartReducer } from "./reducers/cartReducers";
-import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
-import { userSigninReducer } from './reducers/userReducers';
+import {
+  orderCreateReducer,
+  orderDeleteReducer,
+  orderDeliverReducer,
+  orderDetailsReducer,
+  orderListReducer,
+  orderMineListReducer,
+  orderPayReducer,
+} from './reducers/orderReducers';
+import {
+  productCreateReducer,
+  productDeleteReducer,
+  productDetailsReducer,
+  productListReducer,
+  productUpdateReducer,
+} from './reducers/productReducers';
+import {
+  userDetailsReducer,
+  userRegisterReducer,
+  userSigninReducer,
+  userUpdateProfileReducer,
 
+} from './reducers/userReducers';
 
 
 const initialState = {
@@ -16,6 +36,11 @@ const initialState = {
       cartItems: localStorage.getItem('cartItems')
         ? JSON.parse(localStorage.getItem('cartItems'))
         : [],
+        shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
+      paymentMethod: 'PayPal',
+
     },
   };
 const reducer = combineReducers({
@@ -23,6 +48,28 @@ const reducer = combineReducers({
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderMineList: orderMineListReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productDelete: productDeleteReducer,
+    orderList: orderListReducer,
+    orderDelete: orderDeleteReducer,
+    orderDeliver: orderDeliverReducer,
+
+
+
+
+
+
+
+
+    
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
